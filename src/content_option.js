21be4001189e8210969e012a601f27c58f8e1cf6
bsonatunde <onatunde.samuel@gmail.com>
2022-11-03 05:27:59 +0100

    portfolio'
    git commit -m portfolio
    
    git commit -m "portfolio

---
+++ src/content_option.js
@@ -0,0 +1,172 @@
+import mypicture from './assets/images/me.jpg'
+import therapy from './assets/images/Therapy.PNG'
+
+
+const logotext = "Bsonat";
+const meta = {
+    title: "Onatunde Bolaji",
+    description: "I’m Onatunde Bolaji Front end devloper and UI/UX,  I train people how to code and currently base in Nigeria",
+};
+
+const introdata = {
+    title: "I’m Onatunde Bolaji",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I am Ui/Ux Designer",
+    },
+    description: "Translating designs to front-end code and determining developer requirementsRespected Front-End Web Developer with 2-plus years of experience in collaborating withcreative and development teams on the execution of ideas and projects.",
+    your_img_url: mypicture,
+};
+
+const dataabout = {
+    title: "abit about my self",
+    aboutme: "I am a motivated and skilled individual seeking opportunities in Web Development, UI/UX Design and  IT Support. Through completion of the Google UX Design Professional Certificate and Google IT Support Professional Certificate and other related IT certifications. I have developed skills in customer service, operating systems, Figma, Photoshop, Wireframe, Prototype. (including Windows & Linux), system administration, security, website design & development (WordPress/HTML/CSS/JavaScript). My training includes hands-on practice in these areas. I am passionate about continuously learning in IT field and supporting people to solve their technical problems.",
+};
+const worktimeline = [{
+        jobtitle: "Designer/ frontend Developer of Fricanet",
+        where: "Lagos Nigeria",
+        date: "2022",
+    },
+    {
+        jobtitle: "Designer of Aukuwa",
+        where: "Lagos Nigeria",
+        date: "2021",
+    },
+    {
+        jobtitle: "Freelancer",
+        where: "Lagos Nigeria",
+        date: "2019",
+    },
+];
+
+const skills = [{
+        name: "JavaScript",
+        value: 90,
+    },
+    {
+        name: "React js",
+        value: 85,
+    },
+    {
+        name: "Figma",
+        value: 80,
+    },
+    {
+        name: "Adobe XD",
+        value: 60,
+    },
+    {
+        name: "Wordpress",
+        value: 85,
+    },
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: "Google certified UX designer with 2+ years of experience in designing integrated solutions for SAP systems. Highly skilled in developing and redesigning websites and mobile apps by understanding user behavior patterns to bolster sales and annual revenue. Proficient in coordinating with cross-functional teams to manage multiple projects while ensuring timely delivery. Design Foundations of User Experience (UX) Design Start the UX Design Process: Empathize, Define, and Ideate Build Wireframes and Low-Fidelity Prototypes Conduct UX Research and Test Early Concepts High Fidelity Prototype.",
+    },
+    {
+        title: "React Apps",
+        description: "Strong knowledge of basic web technologies such as HTML, CSS, Bootstrap, and others  -depth knowledge of JavaScript, React JS",
+    },
+    {
+        title: "Wordpress Design",
+        description: "WordPress developers are required to have a solid understanding of the application’s core functions. Most commonly, they create custom plugins for WordPress. Programming languages like HTML5, CSS3, JavaScript and PHP may also be required competencies. Design skills, mostly pertaining to color, fonts and font-size, are also often needed.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        desctiption: "Crappo Animation landing page Website    ",
+        link: "https://crypto-crappo.netlify.app/",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        desctiption: "The new House Blog website.",
+        link: "http://newshouse.herokuapp.com/ ",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: therapy,
+        desctiption: "Casestudy of therapix Sunrise App Design.",
+        link: "https://docs.google.com/presentation/d/1RHsdV7YjloIk8J2pdNeOl2XTxJhhwscpIg6KQC9eln8/present?slide=id.g11197727c65_0_310",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "onatunde.samuel@gmail.com",
+    YOUR_FONE: "(+234)813-887-3454",
+    description: "Ikeja Lagos Nigeria",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_s0dquht",
+    YOUR_TEMPLATE_ID: "template_8s4avle",
+    YOUR_USER_ID: "zh3Gt9Yg5sVEnyg4a",
+};
+
+const socialprofils = {
+    github: "https://github.com/bsonatunde",
+    facebook: "https://web.facebook.com/bhinary.jay/",
+    linkedin: "https://www.linkedin.com/in/onatunde-bolaji-b42130100/",
+    twitter: "https://twitter.com/bhinary_jay",
+    youtube: "https://www.youtube.com/channel/UCARJLVqLc7GnUlLtO0oCeIA",
+    twitch: "https://www.twitch.tv/bsonat",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
